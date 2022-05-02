4accae41b71a91747aacda63128d22f3a314901a
YASH <74112721+redwild1066@users.noreply.github.com>
2022-05-03 00:12:11 +0530

    Add files via upload

---
+++ gal.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Indian Army Database</title>
+
+  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="plugins/cubeportfolio/css/cubeportfolio.min.css">
+  <link href="css/nivo-lightbox.css" rel="stylesheet" />
+  <link href="css/nivo-lightbox-theme/default/default.css" rel="stylesheet" type="text/css" />
+  <link href="css/owl.carousel.css" rel="stylesheet" media="screen" />
+  <link href="css/owl.theme.css" rel="stylesheet" media="screen" />
+  <link href="css/animate.css" rel="stylesheet" />
+  <link href="css/style.css" rel="stylesheet">
+
+  <link id="bodybg" href="bodybg/bg1.css" rel="stylesheet" type="text/css" />
+
+  <link id="t-colors" href="color/default.css" rel="stylesheet">
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+  <div id="wrapper">
+
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+      <div class="top-area">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6 col-md-6">
+              <p class="bold text-left">Service Before Self </p>
+            </div>
+            <div class="col-sm-6 col-md-6">
+              <p class="bold text-right">INDIAN ARMY</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container navigation">
+
+        <div class="navbar-header page-scroll">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+          <a class="navbar-brand" href="index.html">
+                    <img src="img/logo.jpg" alt="" width="60" height="50" />
+                </a>
+        </div>
+
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="main.html">Home</a></li>
+            <li><a href="#service">Life in Army</a></li>
+            <li><a href="#doctor">Leaders</a></li>
+            <li><a href="#facilities">Facilities</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="badge custom-badge red pull-right">Extra</span>More <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="gal.php">Gallantry Award</a></li>
+                <li><a href="award.php">Special award</a></li>
+                <li><a href="honour.php">Honours</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+    </nav>
+
+    <section id="intro" class="intro">
+      <div class="intro-content">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="wow fadeInDown" data-wow-offset="0" data-wow-delay="0.1s">
+                  <h2 class="h-ultra"><a href="gal.php"></a>Gallantry awards</a></h2>
+              </div>
+              <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.1s">
+                <h4 class="h-light"> <span class="color">Awardes</span></h4>
+              </div>
+
+            </div>
+            <div class="col-lg-6">
+              <div class="form-wrapper">
+                <div class="wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.2s">
+
+                  
+                      <a href="first.php">
+					  <input type="submit" value="First Category" class="btn btn-skin btn-block btn-lg" style="width:40%;"></a>
+
+
+	                  <a href="award.php"><input type="submit" value="Second Category" class="btn btn-skin btn-block btn-lg" style="width:40%;margin right:30px;">
+                        <p class="lead-footer"></p>
+
+                      </form>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <div class="col-lg-6">
+              <div class="embed-responsive embed-responsive-4by3">
+                <iframe class="embed-responsive-item" src="gallantry.mp4"></iframe>
+              </div>
+            </div>
+    </section>
+
+    <footer>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6 col-md-4">
+            <div class="wow fadeInDown" data-wow-delay="0.1s">
+              <div class="widget">
+                <h5>About Gallantry awards</h5>
+                <p>
+                Gallantry Awards are classified into two Categories <br>
+Gallantry in the Face of Enemy<br>
+Gallantry Other than in the Face of Enemy <br>
+First Category of Gallantry Awards Comprises of the following Awards<br>
+Param Vir Chakra (PVC)<br>
+Mahavir Chakra (MVC)<br>
+Vir Chakra<br>
+Sena Medal, Nao Sena Medal, Vayu Sena Medal<br>
+<br><br>
+
+Second Category of Gallantry Awards Comprises of the following Awards<br>
+Ashok Chakra<br>
+Kirti Chakra<br>
+Shaurya Chakra<br>
+Sena Medal, Nao Sena Medal, Vayu Sena Medal<br>
+</p>
+              </div>
+            </div>
+            
+          </div>
+            
+            
+            
+        
+          
+        </div>
+      </div>
+ 
+  </div>
+  <a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.min.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script src="js/jquery.scrollTo.js"></script>
+  <script src="js/jquery.appear.js"></script>
+  <script src="js/stellar.js"></script>
+  <script src="plugins/cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/nivo-lightbox.min.js"></script>
+  <script src="js/custom.js"></script>
+  <script src="contactform/contactform.js"></script>
+
+</body>
+
+</html>
+ 
